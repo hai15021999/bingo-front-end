854f25f821d654ea84392eb64c14b0bcf46dc562
Casber <61676624+hai15021999@users.noreply.github.com>
2024-01-19 23:48:26 +0700

    change domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     version: '1.1.0.2',
-    domain: 'https://loto-app-back-end.vercel.app', //'http://localhost:3200'
+    domain: 'http://loto-app-back-end.vercel.app', //'http://localhost:3200'
     server: 'vercel',
     apiVersion: 'v1',
     userToken: ''
