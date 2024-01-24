706e7c194f72d70075afa6e6e38a0948c4bb2ecd
Casber <61676624+hai15021999@users.noreply.github.com>
2024-01-24 14:37:54 +0700

    update loto paper

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '1.1.0.3',
+    version: '1.1.0.4',
     // domain: 'https://loto-app-back-end.vercel.app',
     domain: 'http://bingo-casber.southeastasia.cloudapp.azure.com:8080', 
     server: 'vercel',
